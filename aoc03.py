a37cd503e70ce71ae995266f121ea639b3221ca6
Papik Meli <papik@qlac.ch>
2021-12-03 17:45:12 +0100

    Day 3, part 2 completed.

--- aoc03.py
+++ aoc03.py
@@ -5,7 +5,7 @@ from aoc import *
 pd = Debug(True)
 DAY = 3
 SOLVED_1 = True
-SOLVED_2 = False
+SOLVED_2 = True
 
 
 def get_input(filename):
@@ -14,21 +14,36 @@ def get_input(filename):
     return lines.splitlines()
 
 
-def rates(lines):
+def most_common(lines):
+    most_commons = [0] * len(lines[0])
     one_counts = [0] * len(lines[0])
     for line in lines:
         for i, digit in enumerate(line):
             if digit == '1':
                 one_counts[i] += 1
-    print(one_counts)
 
-    gamma_rate = [0] * len(lines[0])
-    epsilon_rate = [0] * len(lines[0])
     for i, count in enumerate(one_counts):
         if count > len(lines) / 2:
-            gamma_rate[i] = 1
-        else:
-            epsilon_rate[i] = 1
+            most_commons[i] = 1
+    return most_commons
+
+
+def most_common_2(remaining, bit):
+    one_count = 0
+    for line in remaining:
+        if line[bit] == '1':
+            one_count += 1
+    if one_count >= len(remaining) / 2:
+        return '1'
+    return '0'
+
+
+def rates(lines):
+    gamma_rate = most_common(lines)
+
+    epsilon_rate = [0] * len(lines[0])
+    for i, most_comm in enumerate(gamma_rate):
+        epsilon_rate[i] = 1 - most_comm
 
     gr = 0
     for digit in gamma_rate:
@@ -41,13 +56,61 @@ def rates(lines):
     return gr, er
 
 
+def count_bit(num1, num2):
+    count = 0
+    while int(num1[count]) == num2[count]:
+        count += 1
+    return count
+
+
 def test1(data):
     gamma_rate, epsilon_rate = rates(data)
     return gamma_rate * epsilon_rate
 
 
+def oxy_rate(data):
+    remaining = data[:]
+    bit = 0
+    while len(remaining) > 1:
+        next_round = []
+        most_comm = most_common_2(remaining, bit)
+        for line in remaining:
+            if line[bit] == most_comm:
+                next_round.append(line)
+        bit += 1
+        remaining = next_round
+    oxy = 0
+    for digit in remaining[0]:
+        oxy *= 2
+        if digit == '1':
+            oxy += 1
+    return oxy
+
+
+def co2_rate(data):
+    remaining = data[:]
+    bit = 0
+    while len(remaining) > 1:
+        next_round = []
+        most_comm = most_common_2(remaining, bit)
+        for line in remaining:
+            if line[bit] != most_comm:
+                next_round.append(line)
+        bit += 1
+        remaining = next_round
+    co2 = 0
+    for digit in remaining[0]:
+        co2 *= 2
+        if digit == '1':
+            co2 += 1
+    return co2
+
+
 def test2(data):
-    return 0
+    oxy = oxy_rate(data)
+    co2 = co2_rate(data)
+    print(oxy, co2)
+    return oxy * co2
 
 
 def part1(data):
@@ -56,7 +119,7 @@ def part1(data):
 
 
 def part2(data):
-    return None
+    return test2(data)
 
 
 if __name__ == '__main__':
@@ -66,9 +129,8 @@ if __name__ == '__main__':
     test_eq('Test 1.1', test1, 198, test_input_1)
     print()
 
-    test_input_2 = [4,5,6]
     print('Test Part 2:')
-    test_eq('Test 2.1', test2, 42, test_input_2)
+    test_eq('Test 2.1', test2, 230, test_input_1)
     print()
 
     data = get_input(f'input{DAY}')
