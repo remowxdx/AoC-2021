1fe793b2ab64d4ab770e8e552fb2c8e6c1952af6
Papik Meli <papik@qlac.ch>
2021-12-05 08:46:14 +0100

    Day 5, part 1 completed.

--- aoc05.py
+++ aoc05.py
@@ -14,8 +14,71 @@ def get_input(filename):
     return lines.splitlines()
 
 
+def parse_input(lines):
+    result = []
+    max_x = 0
+    max_y = 0
+    for line in lines:
+        pos0, pos1 = line.split(' -> ')
+        x0, y0 = [int(coord) for coord in pos0.split(',')]
+        x1, y1 = [int(coord) for coord in pos1.split(',')]
+        max_x = max(max_x, x0)
+        max_x = max(max_x, x1)
+        max_y = max(max_y, y0)
+        max_y = max(max_y, y1)
+        result.append(((x0, y0), (x1, y1)))
+    return result, max_x, max_y
+
+
+def is_horizontal(line):
+    if line[0][0] == line[1][0]:
+        return True
+    return False
+
+
+def is_vertical(line):
+    if line[0][1] == line[1][1]:
+        return True
+    return False
+
+
+def print_board(board):
+    pd("Board:")
+    for line in board:
+        pd("".join([str(v) for v in line]))
+
+
+def draw_lines(lines, max_x, max_y):
+    board = [[0] * (max_x+1) for y in range(max_y+1)]
+    for line in lines:
+        if is_vertical(line):
+            y = line[0][1]
+            x0 = min(line[0][0], line[1][0])
+            x1 = max(line[0][0], line[1][0])
+            for x in range(x0, x1 + 1):
+                board[y][x] += 1
+        if is_horizontal(line):
+            x = line[0][0]
+            y0 = min(line[0][1], line[1][1])
+            y1 = max(line[0][1], line[1][1])
+            for y in range(y0, y1 + 1):
+                board[y][x] += 1
+    return board
+
+
+def count_dangers(board):
+    count = 0
+    for row in board:
+        for vents in row:
+            if vents > 1:
+                count += 1
+    return count
+
+
 def test1(data):
-    return 0
+    vent_lines, max_x, max_y = parse_input(data)
+    board = draw_lines(vent_lines, max_x, max_y)
+    return count_dangers(board)
 
 
 def test2(data):
@@ -23,7 +86,7 @@ def test2(data):
 
 
 def part1(data):
-    return None
+    return test1(data)
 
 
 def part2(data):
@@ -32,9 +95,9 @@ def part2(data):
 
 if __name__ == '__main__':
 
-    test_input_1 = [1,2,3]
+    test_input_1 = get_input('ex5')
     print('Test Part 1:')
-    test_eq('Test 1.1', test1, 42, test_input_1)
+    test_eq('Test 1.1', test1, 5, test_input_1)
     print()
 
     test_input_2 = [4,5,6]
