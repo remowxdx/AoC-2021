2a3c8c5bc9d569dd26088ce907d7f05b0335023f
Papik Meli <papik@qlac.ch>
2021-12-05 12:04:14 +0100

    Added visualizations for day 1 and day 5.

--- aoc05.py
+++ aoc05.py
@@ -2,6 +2,9 @@
 
 from aoc import *
 
+import cairo
+import math
+
 pd = Debug(True)
 DAY = 5
 SOLVED_1 = False
@@ -46,6 +49,28 @@ def print_board(board):
     pd("Board:")
     for line in board:
         pd("".join([str(v) for v in line]))
+
+def draw(lines, board, max_x, max_y):
+    s = cairo.SVGSurface('images/day5.svg', max_x, max_y)
+    c = cairo.Context(s)
+
+    if True:
+        c.set_source_rgba(1.0, 0, 0, 0.3)
+        for line in lines:
+            c.move_to(line[0][0], line[0][1])
+            c.line_to(line[1][0], line[1][1])
+            c.stroke()
+
+# Takes much space and isn't better than the previous
+    if False:
+        for y, row in enumerate(board):
+            for x, vent in enumerate(row):
+                if vent > 0:
+                    c.set_source_rgba(1.0, 0, 0, vent / 5)
+                    c.move_to(x, y)
+                    c.arc(x, y, 1, 0, math.pi)
+                    c.fill()
+    s.finish()
 
 
 def draw_line(board, x0, y0, x1, y1):
@@ -93,6 +118,7 @@ def test1(data):
 def test2(data):
     vent_lines, max_x, max_y = parse_input(data)
     board = draw_all_lines(vent_lines, max_x, max_y)
+    draw(vent_lines, board, max_x, max_y)
     return count_dangers(board)
 
 
