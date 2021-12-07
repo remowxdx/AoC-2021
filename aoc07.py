faaf09760902dbfea5b0a604d4df3231f3195d80
Remo Williams <remo@qlac.ch>
2021-12-07 23:21:21 +0100

    Day 7, graph of cost and crabs.

--- aoc07.py
+++ aoc07.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import cairo
 from aoc import *
 
 pd = Debug(True)
@@ -45,15 +46,61 @@ def test2(data):
     return min(costs)
 
 
+def draw_costs_and_positions(costs, positions, part):
+    surface = cairo.SVGSurface(f'images/day7_{part}.svg', 800, 600)
+    ctx = cairo.Context(surface)
+
+    position_count = {}
+    for position in positions:
+        if position not in position_count:
+            position_count[position] = 0
+        position_count[position] += 1
+    max_count = max(position_count.values())
+
+    max_costs = max(costs)
+    min_cost = min(costs)
+    max_pos = max(positions)
+    scale_pos = 700 / max_pos
+    scale_count = 500 / max_count
+    scale_cost = 500 / max_costs
+
+    ctx.move_to(49, 551)
+    ctx.line_to(750, 551)
+    ctx.stroke()
+    ctx.move_to(49, 551)
+    ctx.line_to(49, 50)
+    ctx.stroke()
+
+    ctx.set_line_width(1)
+    for i in range(max_pos):
+        if costs[i] == min_cost:
+            ctx.set_source_rgb(0.0, 1.0, 0.0)
+        else:
+            ctx.set_source_rgb(1.0, 0.0, 0.0)
+        ctx.move_to(50 + i * scale_pos, 550)
+        ctx.line_to(50 + i * scale_pos, 550 - costs[i] * scale_cost)
+        ctx.stroke()
+
+    ctx.set_source_rgba(0.0, 0.0, 0.0, 0.3)
+    for i, count in position_count.items():
+        ctx.move_to(50 + i * scale_pos, 550)
+        ctx.line_to(50 + i * scale_pos, 550 - count * scale_count)
+    ctx.stroke()
+
+    surface.flush()
+    surface.finish()
+
 def part1(data):
     positions = get_positions(data)
     costs = [fuel_cost(x, positions) for x in range(max(positions))]
+    draw_costs_and_positions(costs, positions, 1)
     return min(costs)
 
 
 def part2(data):
     positions = get_positions(data)
     costs = [fuel_cost2(x, positions) for x in range(max(positions))]
+    draw_costs_and_positions(costs, positions, 2)
     return min(costs)
 
 
