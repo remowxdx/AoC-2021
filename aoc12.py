31db7c26ba7bc16a6a668f343fb1143b6a184f47
Remo Williams <remo@qlac.ch>
2021-12-12 12:10:51 +0100

    Day 12 map visualization.

--- aoc12.py
+++ aoc12.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import cairo
+import math
+
 from aoc import check_solution, save_solution, test_eq
 
 DAY = 12
@@ -22,6 +25,131 @@ def make_caves(lines):
         caves[from_].append(to)
         caves[to].append(from_)
     return caves
+
+
+def link_force(caves_pos, from_, to):
+    from_pos = caves_pos[from_][0]
+    to_pos = caves_pos[to][0]
+    delta_pos = (from_pos[0] - to_pos[0], from_pos[1] - to_pos[1])
+    d = math.sqrt(delta_pos[0] ** 2 + delta_pos[1] ** 2)
+
+    if d == 0:
+        return (0, 0)
+
+    g = 1.0
+    l = 40
+    k = 0.1
+    pos_force = (delta_pos[0] / d ** 1 * g, delta_pos[1] / d ** 1 * g)
+
+    if to in caves_pos[from_][2]:
+        link_force = (k * delta_pos[0] * (l - d) / d, k * delta_pos[1] * (l - d) / d)
+    else:
+        link_force = (0, 0)
+
+    force = sum_forces(pos_force, link_force)
+    return force
+
+
+def sum_forces(force1, force2):
+    return (force1[0] + force2[0], force1[1] + force2[1])
+
+
+def move_caves(caves):
+    caves_pos = {}
+    cols = int(math.sqrt(len(caves)))
+    x = 0
+    y = 0
+    for cave, links in caves.items():
+        if cave == 'start':
+            caves_pos[cave] = ((50, 300), (0, 0), links)
+        elif cave == 'end':
+            caves_pos[cave] = ((750, 300), (0, 0), links)
+        else:
+            caves_pos[cave] = ((100 + x * 50, 50 + y * 50), (0.1, 0), links)
+        y += 1
+        if y > cols:
+            y = 0
+            x += 1
+
+    moving = True
+    while moving:
+        moving = False
+        new_caves_pos = {}
+        for cave, pos_links in caves_pos.items():
+            if cave in ['start', 'end']:
+                new_caves_pos[cave] = pos_links
+                continue
+            pos, vel, links = pos_links
+            force = (0, 0)
+            for cave_1 in caves:
+                if cave == cave_1:
+                    continue
+                force = sum_forces(force, link_force(caves_pos, cave, cave_1))
+            dt = 0.1
+            damp = 0.99
+            if vel[0] ** 2 + vel[1] ** 2 > 0.001:
+                moving = True
+            new_vel = (damp * vel[0] + force[0] * dt, damp * vel[1] + force[1] * dt)
+            new_pos = (pos[0] + new_vel[0] * dt, pos[1] + new_vel[1] * dt)
+            new_caves_pos[cave] = (new_pos, new_vel, links)
+        caves_pos = new_caves_pos
+
+    return caves_pos
+
+
+def draw_caves(caves):
+    surface = cairo.SVGSurface('images/day12.svg', 800, 600)
+    ctx = cairo.Context(surface)
+
+    ctx.select_font_face("Sans Serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+    ctx.set_font_size(10)
+
+    caves_pos = move_caves(caves)
+    left = right = caves_pos['start'][0][0]
+    top = bottom = caves_pos['start'][0][1]
+    for cave, pos_links in caves_pos.items():
+        pos, _vel, links = pos_links
+        if pos[0] < left:
+            left = pos[0]
+        if pos[0] > right:
+            right = pos[0]
+        if pos[1] < top:
+            top = pos[1]
+        if pos[1] > bottom:
+            bottom = pos[1]
+    print(left, right, top, bottom)
+
+    ctx.set_source_rgb(0.0, 0.0, 1.0)
+    for cave, pos_links in caves_pos.items():
+        pos, vel, links = pos_links
+        for link in links:
+            ctx.move_to(pos[0], pos[1])
+            ctx.line_to(caves_pos[link][0][0], caves_pos[link][0][1])
+        ctx.stroke()
+
+    ctx.set_source_rgb(1.0, 0.0, 0.0)
+    for cave, pos_links in caves_pos.items():
+        pos, vel, links = pos_links
+        if cave.islower():
+            radius = 10
+        else:
+            radius = 20
+        ctx.arc(pos[0], pos[1], radius, 0, 2 * math.pi)
+        ctx.fill()
+
+    ctx.set_source_rgb(1.0, 0.0, 0.0)
+    for cave, pos_links in caves_pos.items():
+        pos, vel, links = pos_links
+        ctx.set_source_rgb(0.0, 0.0, 0.0)
+        if cave.islower():
+            up = 12
+        else:
+            up = 20
+        ctx.move_to(pos[0] - 8, pos[1] - up)
+        ctx.show_text(cave)
+
+    surface.flush()
+    surface.finish()
 
 
 def find_paths(caves, from_, to, visited):
@@ -78,6 +206,7 @@ def find_paths_2(caves, from_, to, visited, jolly):
 
 def part1(data):
     caves = make_caves(data)
+    draw_caves(caves)
     paths = find_paths(caves, 'start', 'end', set())
     return len(paths)
 
