e9caa829cfb531316ab51b7276c8e44c6f7b76df
Remo Williams <remo@qlac.ch>
2021-12-16 00:26:47 +0100

    Day 15 with path visualization.

--- aoc15.py
+++ aoc15.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import cairo
 from heapq import heappush, heappop
 from aoc import check_solution, save_solution, test_eq
 
@@ -32,9 +33,21 @@ def find_neighbors(cavern, pos):
     for direction in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
         neighbor = (pos[0] + direction[0], pos[1] + direction[1])
         if neighbor[0] >= 0 and neighbor[1] >= 0:
-            if neighbor[0] < len(cavern) and neighbor[1] < len(cavern[neighbor[0]]):
-                neighbors.append(((end[0] - neighbor[0]) ** 2 + (end[1] - neighbor[1]) ** 2, neighbor))
+            if neighbor[0] <= end[0] and neighbor[1] <= end[1]:
+                neighbors.append((
+                    (end[0] - neighbor[0]) ** 2 + (end[1] - neighbor[1]) ** 2,
+                    neighbor))
     return [pos for dist, pos in sorted(neighbors, key=lambda el: el[0])]
+
+
+def find_lowest_risk_neighbor(lowest_risks, pos):
+    neighbors = []
+    for direction in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        neighbor = (pos[0] + direction[0], pos[1] + direction[1])
+        if neighbor[0] >= 0 and neighbor[1] >= 0:
+            if neighbor in lowest_risks:
+                neighbors.append(neighbor)
+    return min(neighbors, key=lambda nb: lowest_risks[nb])
 
 
 def find_lowest_risks(cavern, start):
@@ -42,46 +55,71 @@ def find_lowest_risks(cavern, start):
     lowest_risks = {start: 0}
     to_visit = []
     heappush(to_visit, (0, start))
-    max_pos = (0, 0)
 
     while len(to_visit) > 0:
         total_risk, pos = heappop(to_visit)
-        if pos[0] + pos[1] >= max_pos[0] + max_pos[1]:
-            max_pos = pos
-            print(pos, lowest_risks[pos], len(to_visit), len(lowest_risks))
         for neighbor in find_neighbors(cavern, pos):
             neighbor_total_risk = cavern[neighbor[0]][neighbor[1]] + total_risk
-            if neighbor not in lowest_risks or lowest_risks[neighbor] > neighbor_total_risk:
+            if neighbor not in lowest_risks:
                 lowest_risks[neighbor] = neighbor_total_risk
                 heappush(to_visit, (neighbor_total_risk, neighbor))
+                continue
+            if lowest_risks[neighbor] > neighbor_total_risk:
+                lowest_risks[neighbor] = neighbor_total_risk
+                heappush(to_visit, (neighbor_total_risk, neighbor))
+                continue
     return lowest_risks
 
 
 def make_big_cavern(small_cavern):
     cavern = []
     for j in range(5):
-        for y, row in enumerate(small_cavern):
+        for row in small_cavern:
             cavern_row = []
             for i in range(5):
-                for x, risk in enumerate(row):
+                for risk in row:
                     cavern_row.append((risk + i + j - 1) % 9 + 1)
             cavern.append(cavern_row)
     return cavern
 
 
+def draw_cavern(cavern, lowest_risks):
+    width, height = len(cavern[0]), len(cavern)
+    surface = cairo.SVGSurface('images/day15.svg', width, height)
+    ctx = cairo.Context(surface)
+
+    max_risk = max(lowest_risks.values())
+    for y in range(height):
+        for x in range(width):
+            risk = lowest_risks[(y, x)] / max_risk
+            ctx.set_source_rgb(risk, 0.0, 1.0 - risk)
+            ctx.rectangle(x, y, 1, 1)
+            ctx.fill()
+
+    ctx.set_source_rgb(0.0, 0.0, 0.0)
+    pos = (height - 1, width - 1)
+    while pos != (0, 0):
+        ctx.rectangle(pos[1], pos[0], 1, 1)
+        pos = find_lowest_risk_neighbor(lowest_risks, pos)
+    ctx.fill()
+
+
+    surface.write_to_png('images/day15.png')
+    surface.flush()
+    surface.finish()
+
+
 def part1(data):
     cavern = make_cavern(data)
     lowest_risks = find_lowest_risks(cavern, (0, 0))
-    # print("\n".join(["".join([f'{lowest_risks[(y, x)]:3}' for x in range(len(cavern[y]))]) for y in range(len(cavern))]))
     return lowest_risks[len(cavern) - 1, len(cavern[0]) - 1]
 
 
 def part2(data):
     small_cavern = make_cavern(data)
     cavern = make_big_cavern(small_cavern)
-    # print(cavern)
-    # print("\n".join(["".join([str(r) for r in row]) for row in cavern]))
     lowest_risks = find_lowest_risks(cavern, (0, 0))
+    draw_cavern(cavern, lowest_risks)
     return lowest_risks[len(cavern) - 1, len(cavern[0]) - 1]
 
 
