1fc71a1422a1f3b31ecde664fbe58629407cfcf5
Remo Williams <remo@qlac.ch>
2021-12-22 20:15:32 +0100

    Day 22, part 1 complete.

--- aoc22.py
+++ aoc22.py
@@ -11,8 +11,58 @@ def get_input(filename):
     return lines.splitlines()
 
 
+def read_instructions(lines):
+    instructions = []
+    for line in lines:
+        instruction, coords = line.split(' ')
+        ranges = coords.split(',')
+        bounds = [range_.split('=')[1] for range_ in ranges]
+        coords = [tuple(int(num) for num in bound.split('..')) for bound in bounds]
+        instructions.append((
+            instruction,
+            coords[0], coords[1], coords[2]))
+    return instructions
+
+
+def execute_instruction(cubes, instruction):
+    print('+', instruction)
+    if instruction[0] == 'on':
+        for x in range(instruction[1][0], instruction[1][1] + 1):
+            if abs(x) > 50:
+                break
+            for y in range(instruction[2][0], instruction[2][1] + 1):
+                if abs(y) > 50:
+                    break
+                for z in range(instruction[3][0], instruction[3][1] + 1):
+                    if abs(z) > 50:
+                        break
+                    cubes.add((x, y, z))
+    else:
+        for x in range(instruction[1][0], instruction[1][1] + 1):
+            if abs(x) > 50:
+                break
+            for y in range(instruction[2][0], instruction[2][1] + 1):
+                if abs(y) > 50:
+                    break
+                for z in range(instruction[3][0], instruction[3][1] + 1):
+                    if abs(z) > 50:
+                        break
+                    if (x, y, z) in cubes:
+                        cubes.remove((x, y, z))
+    return cubes
+
+
+def execute(cubes, instructions):
+    for instruction in instructions:
+        execute_instruction(cubes, instruction)
+    return cubes
+
+
 def part1(data):
-    return None
+    instructions = read_instructions(data)
+    print("\n".join([str(instruction) for instruction in instructions]))
+    cubes = execute(set(), instructions)
+    return len(cubes)
 
 
 def part2(data):
@@ -22,7 +72,7 @@ def part2(data):
 def run_tests():
     test_input_1 = get_input(f'ex{DAY}')
     print('Test Part 1:')
-    test_eq('Test 1.1', part1, 42, test_input_1)
+    test_eq('Test 1.1', part1, 590784, test_input_1)
     print()
 
     print('Test Part 2:')
@@ -54,7 +104,7 @@ def run_part2(solved):
 
 def main():
     run_tests()
-    # run_part1(False)
+    run_part1(True)
     # run_part2(False)
 
 
