f0864547ef60757fde38a9544ca7934644534a2a
Remo Williams <remo@qlac.ch>
2022-01-03 22:45:35 +0100

    Day 23, part 2 complete.

--- aoc23.py
+++ aoc23.py
@@ -288,6 +288,274 @@ def find_min_energy(burrow, initial_amphipods, printer):
     return min_energy[0]
 
 
+def read_positions_2(lines):
+    pos = [(a, b) for b in range(4) for a in range(4)]
+    pos_index = 0
+    positions = {}
+    to_map = {(1, i + 1): (4, i) for i in range(11)}
+    for y, line in enumerate(lines):
+        for x, cell in enumerate(line):
+            if cell in ['A', 'B', 'C', 'D']:
+                positions[pos[pos_index]] = cell
+                to_map[(y, x)] = pos[pos_index]
+                pos_index += 1
+    print('To map:', to_map)
+    return positions, to_map
+
+
+def make_map_2():
+    hallway = {
+        (4, 0): [
+            (0, 0, 3), (0, 1, 4), (0, 2, 5), (0, 3, 6),
+            (1, 0, 5), (1, 1, 6), (1, 2, 7), (1, 3, 8),
+            (2, 0, 7), (2, 1, 8), (2, 2, 9), (2, 3, 10),
+            (3, 0, 9), (3, 1, 10), (3, 2, 11), (3, 3, 12)],
+        (4, 1): [
+            (0, 0, 2), (0, 1, 3), (0, 2, 4), (0, 3, 5),
+            (1, 0, 4), (1, 1, 5), (1, 2, 6), (1, 3, 7),
+            (2, 0, 6), (2, 1, 7), (2, 2, 8), (2, 3, 9),
+            (3, 0, 8), (3, 1, 9), (3, 2, 10), (3, 3, 11)],
+        (4, 2): [],
+        (4, 3): [
+            (0, 0, 2), (1, 0, 2), (0, 1, 3), (1, 1, 3),
+            (0, 2, 4), (1, 2, 4), (0, 3, 5), (1, 3, 5),
+            (2, 0, 4), (2, 1, 5), (3, 0, 6), (3, 1, 7),
+            (2, 2, 6), (2, 3, 7), (3, 2, 8), (3, 3, 9)],
+        (4, 4): [],
+        (4, 5): [
+            (1, 0, 2), (2, 0, 2), (1, 1, 3), (2, 1, 3),
+            (1, 2, 4), (2, 2, 4), (1, 3, 5), (2, 3, 5),
+            (0, 0, 4), (3, 0, 4), (0, 1, 5), (3, 1, 5),
+            (0, 2, 6), (3, 2, 6), (0, 3, 7), (3, 3, 7)],
+        (4, 6): [],
+        (4, 7): [
+            (2, 0, 2), (3, 0, 2), (2, 1, 3), (3, 1, 3),
+            (2, 2, 4), (3, 2, 4), (2, 3, 5), (3, 3, 5),
+            (1, 0, 4), (1, 1, 5), (0, 0, 6), (0, 1, 7),
+            (1, 2, 6), (1, 3, 7), (0, 2, 8), (0, 3, 9)],
+        (4, 8): [],
+        (4, 9): [
+            (3, 0, 2), (3, 1, 3), (2, 0, 4), (2, 1, 5),
+            (3, 2, 4), (3, 3, 5), (2, 2, 6), (2, 3, 7),
+            (1, 0, 6), (1, 1, 7), (0, 0, 8), (0, 1, 9),
+            (1, 2, 8), (1, 3, 9), (0, 2, 10), (0, 3, 11)],
+        (4, 10): [
+            (3, 0, 3), (3, 1, 4), (2, 0, 5), (2, 1, 6),
+            (3, 2, 5), (3, 3, 6), (2, 2, 7), (2, 3, 8),
+            (1, 0, 7), (1, 1, 8), (0, 0, 9), (0, 1, 10),
+            (1, 2, 9), (1, 3, 10), (0, 2, 11), (0, 3, 12)],
+    }
+    burrow = hallway.copy()
+    for pos, costs in hallway.items():
+        # print(pos, costs)
+        for y, x, cost in costs:
+            if (y, x) not in burrow:
+                print('->', y, x)
+                burrow[(y, x)] = []
+            burrow[(y, x)].append((pos[0], pos[1], cost))
+    print('Burrow:', burrow)
+    return burrow
+
+
+def is_reachable_2(amphipods, start, end):
+    if end in amphipods:
+        return False
+    if start[0] == 4:
+        start, end = end, start
+
+    for i in range(start[1]):
+        if (start[0], i) in amphipods:
+            return False
+
+    burrow_to_hallway = [2, 4, 6, 8, ]
+    for i in range(
+            min(burrow_to_hallway[start[0]], end[1]),
+            max(burrow_to_hallway[start[0]], end[1]) + 1):
+        if (4, i) == end:
+            continue
+        if (4, i) in amphipods:
+            return False
+    return True
+
+
+def print_map_2(_burrow, amphipods, to_map):
+    for y in range(7):
+        for x in range(13):
+            if (y, x) in to_map:
+                pos = to_map[(y, x)]
+                if pos in amphipods:
+                    print(amphipods[pos], end='')
+                else:
+                    print('.', end='')
+            else:
+                neighbors = [
+                    (y + 1, x + 1),
+                    (y - 1, x - 1),
+                    (y + 1, x - 1),
+                    (y - 1, x + 1),
+                    (y, x + 1),
+                    (y, x - 1),
+                    (y + 1, x),
+                    (y - 1, x),
+                    ]
+                for pos in neighbors:
+                    if pos in to_map:
+                        print('#', end='')
+                        break
+                else:
+                    print(' ', end='')
+        print()
+
+
+def map_printer_2(burrow, to_map):
+    def partial_map_printer(amphipods):
+        print_map_2(burrow, amphipods, to_map)
+    return partial_map_printer
+
+
+def move_out_2(burrow, amphipods, start, end, prev_energy):
+    # print('Move out 2', start)
+    if not is_reachable_2(amphipods, start, end):
+        return None, amphipods
+    return move_amphipod(burrow, amphipods, start, end, prev_energy)
+
+
+def moves_out_2(burrow, amphipods, start, prev_energy):
+    moves = []
+    for i in range(11):
+        end = (4, i)
+        energy, new_amphipods = move_out_2(
+            burrow, amphipods, start, end, prev_energy)
+        if energy is None:
+            continue
+        moves.append((energy, new_amphipods))
+    return moves
+
+
+def move_home_2(burrow, amphipods, start, prev_energy):
+    # print('Move home', start)
+    amphipod = amphipods[start]
+    home = ord(amphipod) - ord('A')
+
+    if foreigner_in_home(amphipods, amphipod):
+        return None, amphipods
+
+    target = None
+    for i in [0, 1, 2, 3]:
+        inner_pos = (home, i)
+        if not is_reachable_2(amphipods, start, inner_pos):
+            break
+        target = inner_pos
+
+    if target is None:
+        return None, amphipods
+    return move_amphipod(burrow, amphipods, start, target, prev_energy)
+
+
+def foreigner_in_home(amphipods, amphipod):
+    room = ord(amphipod) - ord('A')
+    for i in range(4):
+        pos = (room, i)
+        if pos in amphipods:
+            if amphipods[pos] != amphipod:
+                return True
+    return False
+
+
+def all_amphipod_moves_2(burrow, amphipods, amphipod, start, prev_energy):
+    # At home
+    if is_at_home(amphipod, start):
+        if foreigner_in_home(amphipods, amphipod):
+            return moves_out_2(burrow, amphipods, start, prev_energy)
+        return []
+
+    # In another room
+    if start[0] < 4:
+        return moves_out_2(burrow, amphipods, start, prev_energy)
+
+    # In hallway
+    if foreigner_in_home(amphipods, amphipod):
+        return []
+
+    energy, new_amphipods = move_home_2(
+        burrow, amphipods, start, prev_energy)
+
+    if energy is None or new_amphipods is None:
+        return []
+
+    return [(energy, new_amphipods)]
+
+
+def all_amphipods_moves_2(burrow, amphipods, prev_energy):
+    moves = []
+    for start, amphipod in amphipods.items():
+        moves.extend(all_amphipod_moves_2(
+            burrow, amphipods, amphipod, start, prev_energy))
+    return moves
+
+
+def find_min_energy_2(burrow, initial_amphipods, printer):
+    print(initial_amphipods)
+
+    moves = []
+    moves.append((0, initial_amphipods))
+    ends = []
+
+    count = 0
+
+    # while count < 4:
+    while True:
+        count += 1
+        if len(moves) == 0:
+            break
+
+        if len(moves) == 1:
+            print('-' * 20)
+            printer(moves[0][1])
+            print(moves[0][0])
+            print('-' * 20)
+
+        if count % 100_000 == 0:
+            for cost, status in moves[-1:]:
+                printer(status)
+                print(cost)
+            print(count, len(moves), len(ends), end=' ')
+            if len(ends) > 0:
+                print(ends[-1][0])
+            else:
+                print('None')
+
+        energy, amphipods = moves.pop()
+        if len(ends) > 0 and energy > ends[-1][0]:
+            continue
+
+        if energy is None:
+            raise ValueError('Energy is None.')
+        if len(amphipods) != 16:
+            raise ValueError('Lost amphipod!')
+
+        if amphipods_at_home(amphipods):
+            if len(ends) == 0:
+                ends.append((energy, amphipods))
+            min_energy = min(ends, key=lambda el: el[0])
+            if energy < min_energy[0]:
+                ends.append((energy, amphipods))
+                print(energy, amphipods)
+            continue
+
+        moves.extend(all_amphipods_moves_2(burrow, amphipods, energy))
+
+    print("Steps:", count, "solutions:", len(ends))
+
+    if len(ends) == 0:
+        return 0
+
+    min_energy = min(ends, key=lambda el: el[0])
+    printer(min_energy[1])
+
+    return min_energy[0]
+
+
 def part1(data):
     print()
     print('Input:')
@@ -302,19 +570,29 @@ def part1(data):
 
 
 def part2(data):
-    return data
+    print()
+    print('Input:')
+    print("\n".join(data))
+    burrow = make_map_2()
+    amphipods, to_map = read_positions_2(data)
+    print('Parsed:')
+    printer = map_printer_2(burrow, to_map)
+    printer(amphipods)
+    result = find_min_energy_2(burrow, amphipods, printer)
+    return result
 
 
 def run_tests():
-    test_input_1 = get_input(f'ex{DAY}')
+    # test_input_1 = get_input(f'ex{DAY}')
     test_input_2 = get_input(f'ex{DAY}_2')
+    # test_input_1_2 = get_input(f'ex{DAY}_1_2')
     print('Test Part 1:')
-    test_eq('Test 1.1', part1, 12521, test_input_1)
-    test_eq('Test 1.2', part1, 46, test_input_2)
+    # test_eq('Test 1.1', part1, 12521, test_input_1)
+    # test_eq('Test 1.2', part1, 46, test_input_1_2)
     print()
 
     print('Test Part 2:')
-    test_eq('Test 2.1', part2, 42, test_input_1)
+    test_eq('Test 2.1', part2, 44169, test_input_2)
     print()
 
 
@@ -330,7 +608,7 @@ def run_part1(solved):
 
 
 def run_part2(solved):
-    data = get_input(f'input{DAY}')
+    data = get_input(f'input{DAY}_2')
 
     result2 = part2(data)
     print('Part 2:', result2)
@@ -341,9 +619,9 @@ def run_part2(solved):
 
 
 def main():
-    run_tests()
+    # run_tests()
     # run_part1(True)
-    # run_part2(False)
+    run_part2(True)
 
 
 if __name__ == '__main__':
